a46dd3072e914d747e9b752b5ed340c4df56fe47
Alex Valenzuela <avalenzuela@enrolate.cl>
2022-09-24 18:56:34 -0300

    validations_query

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 # FastAPI
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 
 app = FastAPI()
 
@@ -27,3 +27,14 @@ def home():  # PATH OPERATION FUNCTION
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
+
+
+# Validations: Query Parameters
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(
+        ...,
+    ),
+):
+    return {name: age}
